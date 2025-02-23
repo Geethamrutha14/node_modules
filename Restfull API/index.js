c9a0438330b858ece7e1fbd66edd33fb69cd2d39
Geethamrutha14 <geethamruthapalla4@email.com>
2025-02-23 21:06:29 +0530

    REST FULL API's

---
+++ Restfull API/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const data = require('./MOCK_DATA.json');
+const app = express();
+
+app.get("/api/users", (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data, null, 2));
+});
+
+app.get("/users", (req, res) => {
+    const html = `<ul> ${data.map((user)=> `<li> ${user.first_name} </li>`).join(" ")} </ul>`;
+    res.send(html);
+});
+
+app.get("/api/users/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const user = data.find((user)=> user.id === id);
+    res.setHeader('Content-Type','application/json');
+    res.send(JSON.stringify(user,null,2));
+})
+
+app.listen(8000, () => {
+    console.log("Server started listening on port 8000");
+});
